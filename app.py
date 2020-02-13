2d35283680d0cea28ea78e99fe4a9440c1b6b7e4
dschawel <david.schawel@gmail.com>
2020-02-13 08:29:30 -0800

    David's homework

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
 # YOUR FLASK APP HERE
+from flask import Flask, redirect, render_template, request
+from pymongo import MongoClient
+
+# Database Set-up
+client = MongoClient('mongodb://localhost:27017')
+db = client['operators']
+
+# declare app instance
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/loops')
+def loops():
+    return render_template('loops.html')
+
+@app.route('/operators', methods=('GET', 'POST'))
+def operators():
+    if request.method == 'GET':
+        operators = list(db.operators.find())
+        return render_template('operators.html', operators=operators)
+    elif request.method == 'POST':
+        db.operators.insert_one({
+            'name': request.form['name'],
+            'description': request.form['description'],
+            'symbol': request.form['symbol'],
+            'example': request.form['example'],
+            'uses': request.form['uses'],
+        })
+        return redirect('/operators')
+    
+
+
+# get the server up and running
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
